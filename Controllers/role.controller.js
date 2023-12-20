034ccb142bb53acafd9ae71b0aa08446131d3552
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 19:02:14 +0530

    Update role.controller.js

--- Controllers/role.controller.js
+++ Controllers/role.controller.js
@@ -1,1 +1,21 @@
+const UUID = require('uuid')
+const utils = require('../Utils/response')
+const {STATUS_CODE} = require('../Helpers/StatusCode')
+const CONSTANT = require('../Helpers/Constant')
+const user_role = require('../Models/role.model');
 
+exports.createRole = async (req,res) => {
+    try {
+        const {body} = req
+        const {roleMacName, roleName} = body
+        const roleData = {
+            roleId : UUID.v4(),
+            roleMacName, roleName
+        }
+        const role_data = await user_role(roleData).save();
+        return res.status(200).send({'message':'Role Saved Sucessfully', 'data': role_data, "status" : "200"});
+    }catch(error) {
+        console.log('error in createRole controller',error)
+        return utils.sendResponse(res, STATUS_CODE.INTERNAL_SERVER_ERROR, CONSTANT.SOMETHING_WENT_WRONG)
+    }
+}
