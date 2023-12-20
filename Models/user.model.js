fc6543f8c648b1e73e711cfe7d8de55f3ee20464
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 19:05:50 +0530

    Update user.model.js

--- Models/user.model.js
+++ Models/user.model.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
 
+const UserSchema = new Schema({
+    userId : String,
+    username : String,
+    mobile : String,
+    email : String,
+    password : String,
+    role : String
+})
+
+module.exports = mongoose.model('User', UserSchema, 'User');
