82475b7c0f804fbac02be9758261b56b3dbee617
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:58:03 +0530

    Update user.route.js

--- Route/user.route.js
+++ Route/user.route.js
@@ -1,1 +1,9 @@
+const UserController = require('../Controllers/user.controller')
+const express = require('express')
+const UserRoute = express.Router()
 
+UserRoute.post('/api/users/register', UserController.createUser)
+UserRoute.put('/api/update/users/register/:id', UserController.updateUser)
+UserRoute.delete('/api/delete/users/register/:id', UserController.deleteUser)
+
+module.exports = UserRoute
