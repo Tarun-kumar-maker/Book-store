a267217fa594b0d6f6d2905696438e466087d16f
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:59:17 +0530

    Update book.route.js

--- Route/book.route.js
+++ Route/book.route.js
@@ -1,1 +1,11 @@
+const BookController = require('../Controllers/book.controller')
+const express = require('express')
+const BookRoute = express.Router()
 
+BookRoute.post('/api/books', BookController.createBook);
+BookRoute.get('/api/books', BookController.searchBook);
+BookRoute.get('/api/books/:id', BookController.searchBook);
+BookRoute.put('/api/books/:id', BookController.updateBook);
+BookRoute.delete('/api/books/:id', BookController.deleteBook)
+
+module.exports = BookRoute
