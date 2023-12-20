55d369fb9c78a064fd7d62b94c830e050149923e
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:58:53 +0530

    Update index.js

--- Route/index.js
+++ Route/index.js
@@ -1,1 +1,9 @@
+const bookRoute = require('./book.route')
+const userRoute = require('./user.route')
+const RoleRoute = require('./role.route')
 
+exports.routes = {
+    bookRoute,
+    userRoute,
+    RoleRoute
+}
