03c50d156868e12b5eb6e130a2e075fd8b6cef9a
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:59:49 +0530

    Update User.Queries.js

--- Queries/User.Queries.js
+++ Queries/User.Queries.js
@@ -1,1 +1,23 @@
+const User = require('../Models/user.model')
 
+class UserQueries  {
+    async findByEmail(email) {
+        return User.findOne({email})
+    }
+
+    async createUser(data) {
+        return new User(data).save()
+    }
+
+    async updateUser(data) {
+        const {filter,updateObj} = data
+        return User.updateOne(filter,updateObj)
+    }
+
+    async deleteUser(userId) {
+        return User.deleteOne({_id:userId})
+    }
+
+}
+
+module.exports = new UserQueries()
