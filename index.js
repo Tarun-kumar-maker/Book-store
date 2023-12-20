a51bc7ba2b2a5abc8fb283574125bff5a329ef9d
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:53:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+{
+  "name": "BookStore",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "nodemon index.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "joi": "^17.11.0",
+    "jsonwebtoken": "^9.0.2",
+    "mongoose": "^7.6.2",
+    "nodemon": "^3.0.1",
+    "uuid": "^9.0.1"
+  },
+  "description": ""
+}
