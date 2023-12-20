4529803b3bccbee0a3b6a3a8520a0ca0c6362cf7
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 19:04:51 +0530

    Update book.model.js

--- Models/book.model.js
+++ Models/book.model.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
 
+const BookSchema = new Schema({
+    id : String,
+    login_id: String,
+    title : String,
+    author : String,
+    publishedDate : String,
+    genre : String,
+    price: String
+})
+
+
+module.exports = mongoose.model('Book', BookSchema, 'Book');
