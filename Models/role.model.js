b4b039b4cfb15bcc4016a95783e0888de22b0240
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 19:05:11 +0530

    Update role.model.js

--- Models/role.model.js
+++ Models/role.model.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
 
+const RoleSchema = new Schema({
+    roleId : String,
+    roleMacName : String,
+    roleName : String
+})
+
+module.exports = mongoose.model('Role', RoleSchema, 'Role');
