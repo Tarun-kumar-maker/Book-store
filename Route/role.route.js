3e200700b0463ca25db60546f5fee516dc1b449e
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:58:29 +0530

    Update role.route.js

--- Route/role.route.js
+++ Route/role.route.js
@@ -1,1 +1,7 @@
+const RoleController = require('../Controllers/role.controller')
+const express = require('express')
+const RoleRoute = express.Router()
 
+RoleRoute.post('/api/role', RoleController.createRole);
+
+module.exports = RoleRoute
