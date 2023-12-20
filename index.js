0225b28c687680448c8790d71fef61d163443efa
Tarun-kumar-maker <110539597+Tarun-kumar-maker@users.noreply.github.com>
2023-12-20 18:56:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-{
-  "name": "BookStore",
-  "version": "1.0.0",
-  "main": "index.js",
-  "scripts": {
-    "start": "nodemon index.js"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "body-parser": "^1.20.2",
-    "dotenv": "^16.3.1",
-    "express": "^4.18.2",
-    "joi": "^17.11.0",
-    "jsonwebtoken": "^9.0.2",
-    "mongoose": "^7.6.2",
-    "nodemon": "^3.0.1",
-    "uuid": "^9.0.1"
-  },
-  "description": ""
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const bodyParser = require("body-parser");
+const {routes} = require('./Route/index')
+
+app.use(bodyParser.json());
+app.use(routes.bookRoute);
+app.use(routes.userRoute);
+app.use(routes.RoleRoute);
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
 }
+
+mongoose.connect(process.env.DB_URL,connectionParams)
+  .then(() => {
+    app.listen(process.env.PORT,(err) => {
+        if(err) console.log("error occur while listening",process.env.PORT)
+        console.log("server running successfully")
+    })
+  })
+  .catch((error) => {
+    console.log('error in mongoConnection',error)
+  })
+
+mongoose.connection.on('connected',()=>{
+  console.log('MongoDB is connected')
+})
+
+mongoose.connection.on('disconnected',()=>{
+  console.log('MongoDB is not connected')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
